c29c2f27c42a6b927ec0e3beaf008181b8743358
Sarlas ALLADAYE (dklars) <dklars.dev@gmail.com>
2024-01-19 16:17:47 +0100

    GOOD

--- lib/1_reception/liste_contact.dart
+++ lib/1_reception/liste_contact.dart
@@ -3,10 +3,13 @@ import 'package:dressur/5_autre/autre_profil.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'package:dressur/1_reception/synchronisation_avance.dart';
 import 'package:dressur/5_autre/support_assistance.dart';
+import 'package:dressur/6_notification/liste_notification.dart';
 import 'package:dressur/components/constant.dart';
 import 'package:http/http.dart' as http;
+import 'package:dressur/components/profile_menu_reseau.dart';
 
 class ContactDS {
   final String id;
@@ -83,9 +86,10 @@ class _ContactPageState extends State<ContactPage> {
         _loading = false;
         _contacts = contacts;
         nombreContacts = contacts.length;
+        _filteredContacts =
+            List.from(_contacts); // Initialiser avec tous les contacts
       });
     } else {
-      // ignore: use_build_context_synchronously
       showDialog(
         context: context,
         builder: (BuildContext context) {
@@ -119,6 +123,7 @@ class _ContactPageState extends State<ContactPage> {
     fetchContactDSs();
   }
 
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -130,8 +135,8 @@ class _ContactPageState extends State<ContactPage> {
           style: GoogleFonts.poppins(fontWeight: FontWeight.w600),
         ),
         actions: [
-          const Padding(
-            padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(0, 10, 0, 10),
             child: VerticalDivider(
               width: 0,
               color: Colors.white,
@@ -213,127 +218,189 @@ class _ContactPageState extends State<ContactPage> {
           ),
         ],
       ),
-      body: _loading
-          ? const Center(
-              child: CircularProgressIndicator(),
-            )
-          : ListView.builder(
-              itemCount: _contacts.length,
-              itemBuilder: (BuildContext context, int index) {
-                final contact = _contacts[index];
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              onChanged: (value) {
+                setState(() {
+                  _searchText = value;
+                  _filterContacts();
+                });
+              },
+              decoration: InputDecoration(
+                hintText: 'Rechercher...',
+                prefixIcon: Icon(Icons.search),
+              ),
+            ),
+          ),
+          _loading
+              ? const Center(
+                  child: CircularProgressIndicator(),
+                )
+              : Expanded(
+                  child: ListView.builder(
+                    itemCount: _filteredContacts.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      final contact = _filteredContacts[index];
 
-                return Container(
-                  margin: const EdgeInsets.only(
-                      left: 10, top: 10, right: 10, bottom: 0),
-                  child: Row(
-                    children: [
-                      Container(
-                        height: 60,
-                        width: 60,
-                        decoration: BoxDecoration(
-                          gradient: const LinearGradient(
-                            begin: Alignment.topCenter,
-                            end: Alignment.bottomCenter,
-                            colors: [
-                              Colors.white,
-                              Colors.indigoAccent,
-                              Colors.indigo,
-                            ],
-                          ),
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
+                      return Container(
+                        margin: const EdgeInsets.only(
+                            left: 10, top: 10, right: 10, bottom: 0),
+                        child: Row(
                           children: [
-                            CircleAvatar(
-                              backgroundImage:
-                                  AssetImage("images-pays/${contact.pays}.png"),
+                            Container(
+                              height: 60,
+                              width: 60,
+                              decoration: BoxDecoration(
+                                gradient: const LinearGradient(
+                                  begin: Alignment.topCenter,
+                                  end: Alignment.bottomCenter,
+                                  colors: [
+                                    Colors.white,
+                                    Colors.indigoAccent,
+                                    Colors.indigo,
+                                  ],
+                                ),
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  CircleAvatar(
+                                    backgroundImage: AssetImage(
+                                        "images-pays/${contact.pays}.png"),
+                                  ),
+                                  const SizedBox(height: 3),
+                                  Text(
+                                    contact.pays,
+                                    style: const TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 8,
+                                    ),
+                                  ),
+                                ],
+                              ),
                             ),
-                            const SizedBox(height: 3),
-                            Text(
-                              contact.pays,
-                              style: const TextStyle(
-                                color: Colors.white,
-                                fontSize: 8,
+                            const SizedBox(width: 5),
+                            Expanded(
+                              child: Container(
+                                height: 60,
+                                decoration: BoxDecoration(
+                                  gradient: const LinearGradient(
+                                    begin: Alignment.topCenter,
+                                    end: Alignment.bottomRight,
+                                    colors: [
+                                      Colors.indigo,
+                                      Colors.indigoAccent,
+                                      Colors.white,
+                                      Colors.white,
+                                      Colors.white,
+                                    ],
+                                  ),
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                                padding:
+                                    const EdgeInsets.fromLTRB(15, 0, 15, 0),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    const SizedBox(height: 5),
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
+                                      children: [
+                                        Text(
+                                          contact.pseudo,
+                                          style: const TextStyle(
+                                            fontSize: 14,
+                                            fontWeight: FontWeight.bold,
+                                          ),
+                                        ),
+                                        ElevatedButton.icon(
+                                          style: ElevatedButton.styleFrom(
+                                            backgroundColor: primaryColor,
+                                          ),
+                                          label: Text(
+                                            (langUserPhone == "fr")
+                                                ? "Détails"
+                                                : "Details",
+                                            style: const TextStyle(
+                                              fontSize: 13,
+                                              fontWeight: FontWeight.bold,
+                                            ),
+                                          ),
+                                          icon: const Icon(
+                                            Icons.info,
+                                            size: 13,
+                                          ),
+                                          onPressed: () {
+                                            setState(() {
+                                              uidAutreUser = contact.id;
+                                              addUserOnAutreProfilPage = "non";
+                                            });
+                                            Navigator.of(context).push(
+                                                MaterialPageRoute(
+                                                    builder: (context) =>
+                                                        AutreProfilPage()));
+                                          },
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                ),
                               ),
                             ),
                           ],
                         ),
-                      ),
-                      const SizedBox(width: 5),
-                      Expanded(
-                        child: Container(
-                          // width: 80,
-                          height: 60,
-                          decoration: BoxDecoration(
-                            gradient: const LinearGradient(
-                              begin: Alignment.topCenter,
-                              end: Alignment.bottomRight,
-                              colors: [
-                                Colors.indigo,
-                                Colors.indigoAccent,
-                                Colors.white,
-                                Colors.white,
-                                Colors.white,
-                              ],
-                            ),
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          padding: const EdgeInsets.fromLTRB(15, 0, 15, 0),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              const SizedBox(height: 5),
-                              Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Text(
-                                    contact.pseudo,
-                                    style: const TextStyle(
-                                      fontSize: 14,
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  ),
-                                  ElevatedButton.icon(
-                                    style: ElevatedButton.styleFrom(
-                                      backgroundColor: primaryColor,
-                                    ),
-                                    label: Text(
-                                      (langUserPhone == "fr")
-                                          ? "Détails"
-                                          : "Details",
-                                      style: const TextStyle(
-                                        fontSize: 13,
-                                        fontWeight: FontWeight.bold,
-                                      ),
-                                    ),
-                                    icon: const Icon(
-                                      Icons.info,
-                                      size: 13,
-                                    ),
-                                    onPressed: () {
-                                      setState(() {
-                                        uidAutreUser = contact.id;
-                                        addUserOnAutreProfilPage = "non";
-                                      });
-                                      Navigator.of(context).push(
-                                          MaterialPageRoute(
-                                              builder: (context) =>
-                                                  AutreProfilPage()));
-                                    },
-                                  ),
-                                ],
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ],
+                      );
+                    },
                   ),
-                );
-              },
-            ),
+                ),
+        ],
+      ),
     );
+  }
+
+  void _filterContacts() {
+    if (_searchText.isEmpty) {
+      setState(() {
+        _filteredContacts = List.from(_contacts);
+      });
+    } else {
+      final filteredList = _contacts
+          .where((contact) =>
+              contact.pseudo.toLowerCase().contains(_searchText.toLowerCase()))
+          .toList();
+
+      setState(() {
+        _filteredContacts = filteredList;
+      });
+
+      if (_filteredContacts.isEmpty) {
+        showDialog(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: Text((langUserPhone == "fr")
+                  ? "Aucune correspondance"
+                  : "No match"),
+              content: Text((langUserPhone == "fr")
+                  ? "Aucun contact trouvé."
+                  : "No contacts found."),
+              actions: <Widget>[
+                TextButton(
+                  child: Text('OK'),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ],
+            );
+          },
+        );
+      }
+    }
   }
 }
