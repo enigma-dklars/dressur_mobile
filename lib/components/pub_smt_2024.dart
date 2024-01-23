24692223c7c32bead38a7e33a415f7646afca7e5
Sarlas ALLADAYE (dklars) <dklars.dev@gmail.com>
2024-01-23 13:26:46 +0100

    correction du probleme au clique sur la pub, la page charge

--- lib/components/pub_smt_2024.dart
+++ lib/components/pub_smt_2024.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 
 Widget SpecialPub() {
   String imageUrl =
@@ -6,34 +8,20 @@ Widget SpecialPub() {
   String targetUrl = "https://www.bluelife.tech/summer-tech-2024";
   return Padding(
     padding: const EdgeInsets.only(left: 10, top: 5, right: 10, bottom: 5),
-    child: Center(
-      child: GestureDetector(
-        onTap: () async {
-          final Uri _url = Uri.parse(targetUrl);
-          if (!await launchUrl(_url, mode: LaunchMode.externalApplication)) {
-            throw 'Could not launch $_url';
-          }
-        },
-        child: ClipRRect(
-          borderRadius: BorderRadius.circular(5),
-          child: Image.network(
-            imageUrl,
-            fit: BoxFit.cover,
-          ),
+    child: GestureDetector(
+      onTap: () async {
+        final Uri _url = Uri.parse(targetUrl);
+        if (!await launchUrl(_url, mode: LaunchMode.externalApplication)) {
+          throw 'Could not launch $_url';
+        }
+      },
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(5),
+        child: Image.network(
+          imageUrl,
+          fit: BoxFit.cover,
         ),
       ),
     ),
   );
-}
-
-Future<bool> launchUrl(Uri url, {LaunchMode mode = LaunchMode.browser}) async {
-  // Simule la logique de lancement d'URL
-  // Remplacez cela par votre propre logique de lancement d'URL
-  print('Lancement de l\'URL : $url');
-  return true;
-}
-
-enum LaunchMode {
-  browser,
-  externalApplication,
 }
