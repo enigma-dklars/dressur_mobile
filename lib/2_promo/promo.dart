3120956fe9f784f6e298390e8d09edd79f3d050e
Sarlas ALLADAYE (dklars) <dklars.dev@gmail.com>
2024-05-21 11:55:19 +0100

    masquer la campage sms

--- lib/2_promo/promo.dart
+++ lib/2_promo/promo.dart
@@ -542,134 +542,134 @@ class _BoostPageState extends State<BoostPage> {
                 ),
               ),
               DressurDivider(),
-              Card(
-                margin: const EdgeInsets.only(
-                    left: 10, top: 5, right: 10, bottom: 5),
-                child: Container(
-                  padding: const EdgeInsets.fromLTRB(10, 5, 10, 5),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      Text(
-                        (langUserPhone == "fr")
-                            ? "Campagne SMS"
-                            : "SMS Campaign",
-                        style: GoogleFonts.poppins(
-                          color: primaryColor,
-                          fontWeight: FontWeight.w400,
-                          fontSize: 24,
-                        ),
-                      ),
-                      const SizedBox(height: 5),
-                      Text(
-                        (langUserPhone == "fr")
-                            ? "Grâce à notre Campagne SMS, Faites la promotion de vos produits et services."
-                            : "Thanks to our SMS Campaign, promote your products and services.",
-                        style: GoogleFonts.poppins(
-                          fontWeight: FontWeight.w300,
-                          fontSize: 14,
-                        ),
-                      ),
-                      const SizedBox(height: 10),
-                      Text(
-                        (langUserPhone == "fr")
-                            ? "Ce service est actuellement en maintenance. Vous serez notifié dès lors qu'elle sera de nouveau disponible. Merci pour votre compréhension et votre confiance."
-                            : "This service is currently under maintenance. You will be notified when it is available again. Thank you for your understanding and trust.",
-                        style: GoogleFonts.poppins(
-                          color: Colors.red,
-                          fontWeight: FontWeight.w700,
-                          fontSize: 15,
-                        ),
-                      ),
-                      const SizedBox(height: 5),
-                      // Row(
-                      //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      //   children: [
-                      //     Column(
-                      //       children: [
-                      //         SizedBox(
-                      //           width: MediaQuery.of(context).size.width * 0.42,
-                      //           child: ElevatedButton(
-                      //               style: ElevatedButton.styleFrom(
-                      //                 backgroundColor: primaryColor,
-                      //                 shape: const StadiumBorder(),
-                      //                 padding: const EdgeInsets.symmetric(
-                      //                   vertical: 10,
-                      //                 ),
-                      //               ),
-                      //               child: Text(
-                      //                 (langUserPhone == "fr")
-                      //                     ? "Démarrer"
-                      //                     : "To start up",
-                      //                 style: GoogleFonts.poppins(
-                      //                   color: Colors.white,
-                      //                 ),
-                      //               ),
-                      //               onPressed: () {
-                      //                 warningNoti(
-                      //                     (langUserPhone == "fr")
-                      //                         ? "Désoler"
-                      //                         : "Sorry",
-                      //                     (langUserPhone == "fr")
-                      //                         ? "Ce service est momentanément indisponible"
-                      //                         : "This service is temporarily unavailable",
-                      //                     context);
-                      //                 // Navigator.push(
-                      //                 //   context,
-                      //                 //   MaterialPageRoute(
-                      //                 //       builder: (context) =>
-                      //                 //           NewBoostContactPage()),
-                      //                 // );
-                      //               }),
-                      //         )
-                      //       ],
-                      //     ),
-                      //     Column(
-                      //       children: [
-                      //         SizedBox(
-                      //           width: MediaQuery.of(context).size.width * 0.42,
-                      //           child: ElevatedButton(
-                      //               style: ElevatedButton.styleFrom(
-                      //                 backgroundColor: secondaryColor,
-                      //                 shape: const StadiumBorder(),
-                      //                 padding: const EdgeInsets.symmetric(
-                      //                   vertical: 10,
-                      //                 ),
-                      //               ),
-                      //               child: Text(
-                      //                 (langUserPhone == "fr")
-                      //                     ? "Voir la liste"
-                      //                     : "See the list",
-                      //                 style: GoogleFonts.poppins(
-                      //                   color: Colors.white,
-                      //                 ),
-                      //               ),
-                      //               onPressed: () {
-                      //                 warningNoti(
-                      //                     (langUserPhone == "fr")
-                      //                         ? "Désoler"
-                      //                         : "Sorry",
-                      //                     (langUserPhone == "fr")
-                      //                         ? "Ce service est momentanément indisponible"
-                      //                         : "This service is temporarily unavailable",
-                      //                     context);
-                      //                 // Navigator.push(
-                      //                 //   context,
-                      //                 //   MaterialPageRoute(
-                      //                 //       builder: (context) =>
-                      //                 //           ListeBoostContactPage()),
-                      //                 // );
-                      //               }),
-                      //         )
-                      //       ],
-                      //     ),
-                      //   ],
-                      // )
-                    ],
-                  ),
-                ),
-              ),
-              DressurDivider(),
+              // Card(
+              //   margin: const EdgeInsets.only(
+              //       left: 10, top: 5, right: 10, bottom: 5),
+              //   child: Container(
+              //     padding: const EdgeInsets.fromLTRB(10, 5, 10, 5),
+              //     child: Column(
+              //       crossAxisAlignment: CrossAxisAlignment.stretch,
+              //       children: [
+              //         Text(
+              //           (langUserPhone == "fr")
+              //               ? "Campagne SMS"
+              //               : "SMS Campaign",
+              //           style: GoogleFonts.poppins(
+              //             color: primaryColor,
+              //             fontWeight: FontWeight.w400,
+              //             fontSize: 24,
+              //           ),
+              //         ),
+              //         const SizedBox(height: 5),
+              //         Text(
+              //           (langUserPhone == "fr")
+              //               ? "Grâce à notre Campagne SMS, Faites la promotion de vos produits et services."
+              //               : "Thanks to our SMS Campaign, promote your products and services.",
+              //           style: GoogleFonts.poppins(
+              //             fontWeight: FontWeight.w300,
+              //             fontSize: 14,
+              //           ),
+              //         ),
+              //         const SizedBox(height: 10),
+              //         Text(
+              //           (langUserPhone == "fr")
+              //               ? "Ce service est actuellement en maintenance. Vous serez notifié dès lors qu'elle sera de nouveau disponible. Merci pour votre compréhension et votre confiance."
+              //               : "This service is currently under maintenance. You will be notified when it is available again. Thank you for your understanding and trust.",
+              //           style: GoogleFonts.poppins(
+              //             color: Colors.red,
+              //             fontWeight: FontWeight.w700,
+              //             fontSize: 15,
+              //           ),
+              //         ),
+              //         const SizedBox(height: 5),
+              //         // Row(
+              //         //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              //         //   children: [
+              //         //     Column(
+              //         //       children: [
+              //         //         SizedBox(
+              //         //           width: MediaQuery.of(context).size.width * 0.42,
+              //         //           child: ElevatedButton(
+              //         //               style: ElevatedButton.styleFrom(
+              //         //                 backgroundColor: primaryColor,
+              //         //                 shape: const StadiumBorder(),
+              //         //                 padding: const EdgeInsets.symmetric(
+              //         //                   vertical: 10,
+              //         //                 ),
+              //         //               ),
+              //         //               child: Text(
+              //         //                 (langUserPhone == "fr")
+              //         //                     ? "Démarrer"
+              //         //                     : "To start up",
+              //         //                 style: GoogleFonts.poppins(
+              //         //                   color: Colors.white,
+              //         //                 ),
+              //         //               ),
+              //         //               onPressed: () {
+              //         //                 warningNoti(
+              //         //                     (langUserPhone == "fr")
+              //         //                         ? "Désoler"
+              //         //                         : "Sorry",
+              //         //                     (langUserPhone == "fr")
+              //         //                         ? "Ce service est momentanément indisponible"
+              //         //                         : "This service is temporarily unavailable",
+              //         //                     context);
+              //         //                 // Navigator.push(
+              //         //                 //   context,
+              //         //                 //   MaterialPageRoute(
+              //         //                 //       builder: (context) =>
+              //         //                 //           NewBoostContactPage()),
+              //         //                 // );
+              //         //               }),
+              //         //         )
+              //         //       ],
+              //         //     ),
+              //         //     Column(
+              //         //       children: [
+              //         //         SizedBox(
+              //         //           width: MediaQuery.of(context).size.width * 0.42,
+              //         //           child: ElevatedButton(
+              //         //               style: ElevatedButton.styleFrom(
+              //         //                 backgroundColor: secondaryColor,
+              //         //                 shape: const StadiumBorder(),
+              //         //                 padding: const EdgeInsets.symmetric(
+              //         //                   vertical: 10,
+              //         //                 ),
+              //         //               ),
+              //         //               child: Text(
+              //         //                 (langUserPhone == "fr")
+              //         //                     ? "Voir la liste"
+              //         //                     : "See the list",
+              //         //                 style: GoogleFonts.poppins(
+              //         //                   color: Colors.white,
+              //         //                 ),
+              //         //               ),
+              //         //               onPressed: () {
+              //         //                 warningNoti(
+              //         //                     (langUserPhone == "fr")
+              //         //                         ? "Désoler"
+              //         //                         : "Sorry",
+              //         //                     (langUserPhone == "fr")
+              //         //                         ? "Ce service est momentanément indisponible"
+              //         //                         : "This service is temporarily unavailable",
+              //         //                     context);
+              //         //                 // Navigator.push(
+              //         //                 //   context,
+              //         //                 //   MaterialPageRoute(
+              //         //                 //       builder: (context) =>
+              //         //                 //           ListeBoostContactPage()),
+              //         //                 // );
+              //         //               }),
+              //         //         )
+              //         //       ],
+              //         //     ),
+              //         //   ],
+              //         // )
+              //       ],
+              //     ),
+              //   ),
+              // ),
+              // DressurDivider(),
               const SizedBox(height: 5),
               SociauxPage(),
               const SizedBox(height: 5),
