989da01e343c2fc587d60f3b05f011d2f0b684ee
Sarlas ALLADAYE (dklars) <dklars.dev@gmail.com>
2024-11-05 08:22:16 +0100

    MAJ FOR SAN NEXT ---

--- lib/3_actu/actu.dart
+++ lib/3_actu/actu.dart
@@ -2,7 +2,6 @@
 
 import 'dart:async';
 import 'dart:convert';
-import 'dart:developer';
 import 'dart:io';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:dressur/1_reception/liste_contact.dart';
