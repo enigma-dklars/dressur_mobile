e2139cece018f8eaaa0996f450debbfd0384c79c
Sarlas ALLADAYE (dklars) <dklars.dev@gmail.com>
2024-11-08 16:34:13 +0100

    remove importe inutile

--- lib/8_admin/admin.dart
+++ lib/8_admin/admin.dart
@@ -1,11 +1,9 @@
 // ignore_for_file: unnecessary_brace_in_string_interps
 
-import 'package:dressur/components/111_generaleApiDomaine.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:dressur/components/constant.dart';
 import 'package:http/http.dart' as http;
-import 'package:url_launcher/url_launcher.dart';
 
 class AdministrationPage extends StatefulWidget {
   AdministrationPage({Key? key}) : super(key: key);
