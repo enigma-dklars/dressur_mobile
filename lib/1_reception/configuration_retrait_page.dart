7c605284b653725e57ec7615a65f5342d0f4c303
Sarlas ALLADAYE (dklars) <dklars.dev@gmail.com>
2026-02-14 22:59:52 +0100

    quelques cooorections encores

--- lib/1_reception/configuration_retrait_page.dart
+++ lib/1_reception/configuration_retrait_page.dart
@@ -1,7 +1,5 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
-import 'dart:developer';
-
 import 'package:dressur/components/constant.dart';
 import 'package:dressur/components/noti.dart';
 import 'package:flutter/material.dart';
