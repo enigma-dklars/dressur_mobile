d1a9bae49539b3cd613c74bd56faa10f8f724a4d
Sarlas ALLADAYE (dklars) <dklars.dev@gmail.com>
2026-02-20 07:48:22 +0100

    V 1.1.2+16

--- lib/components/constant.dart
+++ lib/components/constant.dart
@@ -18,7 +18,7 @@ import 'package:share_plus/share_plus.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:http/http.dart' as http;
 
-const versionApp = '1.1.0';
+const versionApp = '1.1.2';
 const oldDatabaseName = 'onze_dressur.db';
 const nowDataBaseName = 'douze_dressur.db';
 bool modeReconnaissanceContactArrierePlan = false;
@@ -99,7 +99,7 @@ var modeMotDePasseOublier = false;
 var mailConnexion = "";
 var textChargementEvolution = "Chargement ...";
 var addUserOnAutreProfilPage = "oui";
-var myDressurVersion = '1.1.0';
+var myDressurVersion = '1.1.2';
 var lesPublicites;
 var uidAutreUser;
 var uidUser;
