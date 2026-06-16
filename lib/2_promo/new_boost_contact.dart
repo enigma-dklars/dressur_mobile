9b216f9c8fc881f2bf7f3173baeba6059f56cec2
ALLADAYE Sarlas <73697091+dklars@users.noreply.github.com>
2026-06-16 22:19:06 +0100

    feat: notification bienvenue 24h après inscription si pas de boost

--- lib/2_promo/new_boost_contact.dart
+++ lib/2_promo/new_boost_contact.dart
@@ -245,6 +245,7 @@ class _RegisterFormState extends State<RegisterForm> {
           var data1 = await response.stream.bytesToString();
           var data = convert.jsonDecode(data1);
           if (data["error"] == false) {
+            cancelBoostReminderNotification();
             setState(() {
               _desactive = false;
               ScaffoldMessenger.of(context).showSnackBar(SnackBar(
