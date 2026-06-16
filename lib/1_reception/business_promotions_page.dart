197081d794a1e378580b248025c9ed559eab9f43
ALLADAYE Sarlas <73697091+dklars@users.noreply.github.com>
2026-06-16 13:46:18 +0100

    fix: ajouter color: primaryColor aux CircularProgressIndicator sans couleur

--- lib/1_reception/business_promotions_page.dart
+++ lib/1_reception/business_promotions_page.dart
@@ -389,7 +389,7 @@ class _BusinessPromotionsPageState extends State<BusinessPromotionsPage> {
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
             return const Center(
-              child: CircularProgressIndicator(),
+              child: CircularProgressIndicator(color: primaryColor),
             );
           } else if (snapshot.hasError) {
             return Center(
