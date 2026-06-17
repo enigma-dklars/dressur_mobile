789e552ef851a7c75884ff8fac6e84e25e087b98
Sarlas ALLADAYE (dklars) <dklars.dev@gmail.com>
2026-06-17 15:52:46 +0100

    chore: update app version to 1.1.5 and add app_links plugin support

--- lib/components/constant.dart
+++ lib/components/constant.dart
@@ -18,7 +18,7 @@ import 'package:share_plus/share_plus.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:http/http.dart' as http;
 
-const versionApp = '1.1.4';
+const versionApp = '1.1.5';
 const oldDatabaseName = 'one_dressur.db';
 const nowDataBaseName = 'two_dressur.db';
 bool modeReconnaissanceContactArrierePlan = false;
@@ -100,7 +100,7 @@ var modeMotDePasseOublier = false;
 var mailConnexion = "";
 var textChargementEvolution = "Chargement ...";
 var addUserOnAutreProfilPage = "oui";
-var myDressurVersion = '1.1.4';
+var myDressurVersion = '1.1.5';
 var lesPublicites;
 var uidAutreUser;
 var uidUser;
