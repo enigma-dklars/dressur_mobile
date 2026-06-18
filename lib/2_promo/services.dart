031af1beacde077ccc57bc060c94cd2c10e3d23e
ALLADAYE Sarlas <73697091+dklars@users.noreply.github.com>
2026-06-18 23:42:39 +0100

    fix(admin): séparateur AppBar identique à celui de la notif (white)

--- lib/2_promo/services.dart
+++ lib/2_promo/services.dart
@@ -84,7 +84,7 @@ class _BoostPageState extends State<BoostPage> {
             const Padding(
               padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
               child: VerticalDivider(
-                  width: 0, color: Colors.white38, thickness: 1),
+                  width: 0, color: Colors.white, thickness: 1),
             ),
           ],
           IconButton(
