c566d41efbabc537f722667100d1cafbac03734e
ALLADAYE Sarlas <73697091+dklars@users.noreply.github.com>
2026-06-18 23:42:37 +0100

    fix(admin): séparateur AppBar identique à celui de la notif (white)

--- lib/1_reception/reception.dart
+++ lib/1_reception/reception.dart
@@ -44,7 +44,7 @@ class _ReceptionPageState extends State<ReceptionPage> {
               const Padding(
                 padding: EdgeInsets.fromLTRB(0, 10, 0, 10),
                 child: VerticalDivider(
-                    width: 0, color: Colors.white38, thickness: 1),
+                    width: 0, color: Colors.white, thickness: 1),
               ),
             ],
             IconButton(
