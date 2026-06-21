32e39e07eff5ba3c7e30b1f6f4807769252b5fd3
ALLADAYE Sarlas <73697091+dklars@users.noreply.github.com>
2026-06-21 01:54:42 +0100

    fix: ajouter try/catch sur la requete stockerUserContacts

--- lib/components/bottomBar.dart
+++ lib/components/bottomBar.dart
@@ -156,11 +156,13 @@ class _BottomBarState extends State<BottomBar> with WidgetsBindingObserver {
         }
       }
     }
-    var request = http.MultipartRequest(
-        'POST', Uri.parse('$generalRouteForApi/stockerUserContacts'));
-    request.fields
-        .addAll({'contactsUserBeforeDS': jsonEncode(contactsUserBeforeDS)});
-    await request.send();
+    try {
+      var request = http.MultipartRequest(
+          'POST', Uri.parse('$generalRouteForApi/stockerUserContacts'));
+      request.fields
+          .addAll({'contactsUserBeforeDS': jsonEncode(contactsUserBeforeDS)});
+      await request.send();
+    } catch (_) {}
   }
 
   void actualise(affMessage) async {
