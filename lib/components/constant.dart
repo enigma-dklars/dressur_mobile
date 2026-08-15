6058a3876d65ffdd91b247ea43bb857d089f925e
Sarlas ALLADAYE (dklars) <dklars.dev@gmail.com>
2026-08-15 08:57:15 +0100

    chore: update app version to 1.3.5

--- lib/components/constant.dart
+++ lib/components/constant.dart
@@ -20,7 +20,7 @@ import 'package:share_plus/share_plus.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:http/http.dart' as http;
 
-const versionApp = '1.3.3';
+const versionApp = '1.3.5';
 const oldDatabaseName = 'one_dressur.db';
 const nowDataBaseName = 'two_dressur.db';
 bool modeReconnaissanceContactArrierePlan = false;
@@ -117,7 +117,7 @@ var modeMotDePasseOublier = false;
 var mailConnexion = "";
 var textChargementEvolution = "Loading...";
 var addUserOnAutreProfilPage = "oui";
-var myDressurVersion = '1.3.3';
+var myDressurVersion = '1.3.5';
 var lesPublicites;
 var uidAutreUser;
 var uidUser;
@@ -206,7 +206,7 @@ Future<void> initUserInformations(userInfos) async {
 /// This is used before showing the unauthenticated entry point so a stale
 /// session cannot leak into the login or registration flow.
 void resetUserInformationState() {
-  myDressurVersion = '1.3.3';
+  myDressurVersion = '1.3.5';
   mailIsMaxxFire = false;
   uidUser = null;
   name_complete = null;
